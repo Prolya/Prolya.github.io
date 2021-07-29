921b8e29f31ac1d35b7e5ef314aa93c9971d596c
Permyakova Olga <permyakovao.a.16@gmail.com>
2021-07-30 01:29:09 +0700

    fix

--- js/index.js
+++ js/index.js
@@ -2,14 +2,8 @@ function onButtonClickHandler() {
     console.log('Меня нажали!');
    const block1 = document.getElementById('block1');
    const block2 = document.getElementById('block2');
-
-   //меняет цвет первого блока
+   // меняет цвет первого блока
    block1.style.backgroundColor = 'blue';
-
-
+   // убираем блок 2
    block2.style.visibility = 'hidden';
-
-
-
-
 }
