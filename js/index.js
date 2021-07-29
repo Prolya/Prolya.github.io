57680a8aa0fa2b19123004cb805e827261c211c2
Permyakova Olga <permyakovao.a.16@gmail.com>
2021-07-30 01:17:32 +0700

    сделала тестовое задание

--- js/index.js
+++ js/index.js
@@ -1,2 +1,15 @@
-const t = 'Some name';
-console.log(t);
+function onButtonClickHandler() {
+    console.log('Меня нажали!');
+   const block1 = document.getElementById('block1');
+   const block2 = document.getElementById('block2');
+
+   //меняет цвет первого блока
+   block1.style.backgroundColor = 'blue';
+
+
+   block2.style.visibility = 'hidden';
+
+
+
+
+}
